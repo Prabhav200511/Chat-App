f2b01f9121d628096c6553db633ed0216965c379
Prabhav200511 <manikwarkrishnaprabhav@gmail.com>
2025-06-22 01:02:04 +0530

    check2

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,13 +23,14 @@ app.use(cors({
 app.use("/app/auth",authRoutes);
 app.use("/app/messages",messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/:path(*)", (req,res) => {
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+    app.get("/*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
+
 
 const PORT = process.env.PORT||5000
 
