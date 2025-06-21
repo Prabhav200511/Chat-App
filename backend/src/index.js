aec1989a187e3ca8b2a206c69a39edacdf71e693
Prabhav200511 <manikwarkrishnaprabhav@gmail.com>
2025-06-22 01:14:55 +0530

    check3

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,12 +23,12 @@ app.use(cors({
 app.use("/app/auth",authRoutes);
 app.use("/app/messages",messageRoutes);
 
-if(process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/*", (req, res) => {
+    app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
+});
 }
 
 
